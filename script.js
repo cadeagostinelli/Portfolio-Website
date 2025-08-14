ec4ee15394d30e57bc26997678fdb4032e25c098
cadea <cadea@CAgostinelli.localdomain>
2025-08-14 15:00:24 -0500

    Updated information to be relevant. Fixed few core issues with the website, like unadjusted boxes and centering issues.

--- script.js
+++ script.js
@@ -52,7 +52,20 @@ function showDescription(job) {
     switch (job) {
         case 'job1':
             descriptionHTML = `
-                <h2 class="job-title">Software Engineer Intern<span class="company">@ Fractal Networks</span></h2>
+            <h2 class="job-title">Software Engineer Intern<span class="company">@ Volvo Group</span></h2>
+            <p class="dates">May 2025 - Dec 2025</p>
+            <ul class="job-responsibilities">
+                <li>Developed and patenting a real-time system to be deployed on vehicles to estimate cornering stiffness of tires using Recursive Least Squares algorithm, reducing reliance on $10K+ tire testing procedures</li>
+                <li>Automated weather and temperature related deployment and analysis of vehicles using the open source library Meteostat along with Jenkins</li>
+                <li>Built an internal data science tool utilizing Python and OpenAI API that allows engineers to quickly find statistics, correlations, and graph results from vehicle data</li>
+                <li>Automated tire analysis workflows in the company's internal software using Python, significantly reducing manual testing time for mechanical engineers</li>
+                <li>Developed scalable Python scripts to batch-process and extract key performance statistics from thousands of log files using Pandas</li>
+            </ul>
+        `;
+        break;
+        case 'job2':
+            descriptionHTML = `
+                <h2 class="job-title">Software Engineer Intern<span class="company">@ Hintjen</span></h2>
                 <p class="dates">Jan 2024 - Apr 2024</p>
                 <ul class="job-responsibilities">
                     <li>Collaborated with a team and led the development of over 50% of the backend production code for an event social media app utilizing 
@@ -61,13 +74,13 @@ function showDescription(job) {
                     including pull requests and resolving merge conflicts</li>
                     <li>Implemented notification and phone verification functionality of the app using Twilio API and developed REST APIs 
                     for seamless communication between frontend and backend services</li>
-                    <li>Developed unit tests for Fractal Network’s extensive systems and clients, achieving 90% average code coverage</li>
-                    <li>Improved Fractal Network’s asynchronous client by developing over 60+ unit tests with Python while collaborating with 
+                    <li>Developed unit tests for Hintjen’s extensive systems and clients, achieving 90% average code coverage</li>
+                    <li>Improved Hintjen’s asynchronous client by developing over 60+ unit tests with Python while collaborating with 
                     senior software engineers to conduct code reviews and reimplement the client’s infrastructure</li>
                 </ul>
             `;
             break;
-        case 'job2':
+        case 'job3':
             descriptionHTML = `
                 <h2 class="job-title">STEM Teacher <span class="company">@ Computer Explorers</span></h2>
                 <p class="dates">May 2022 - Aug 2022</p>
